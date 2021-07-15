84a77c6118dc612b83c989e3bfc144439df42970
Rola-H <rolahararah@gmail.com>
2021-07-15 15:43:42 +0300

    make elements invisible ToDo page>Relates#11

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const writeTasks=document.getElementsByClassName('writeTasks')
+writeTasks[0].style.display = "none";
+
+const search=document.getElementsByClassName('search')
+search[0].style.display = "none";
+
+const properties=document.getElementsByClassName('properties')
+properties[0].style.display = "none";
