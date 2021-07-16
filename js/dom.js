abdef8b76e147cd2a1cad7489c7f4dd65e864a83
adnan-afana <adnanafana0@gmail.com>
2021-07-16 23:45:49 +0300

    run the FAB btn to save the user to-do in local storage Relates #24

---
+++ js/dom.js
@@ -0,0 +1,9 @@
+// Document Elements
+const addTaskButton = document.getElementById("addTask");
+const writeTasks = document.getElementsByClassName("writeTasks");
+const properties = document.getElementsByClassName("properties");
+const submitTask = document.getElementById("saveTask");
+writeTasks[0].style.display = "none";
+// Event Listeners
+addTaskButton.addEventListener("click", hideFab);
+submitTask.addEventListener("click", saveToLocal);
--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
-const writeTasks=document.getElementsByClassName('writeTasks')
-writeTasks[0].style.display = "none";
-
-const search=document.getElementsByClassName('search')
+const search = document.getElementsByClassName("search");
 search[0].style.display = "none";
 
-const properties=document.getElementsByClassName('properties')
-properties[0].style.display = "none";
+function hideFab() {
+  if ((writeTasks[0].style.display = "none")) {
+    writeTasks[0].style.display = "block";
+    addTaskButton[0].style.display = "none";
+  }
+}
+function realTime() {
+  const date = new Date();
+  const dateHour = date.getHours();
+  const dateMin = date.getMinutes();
+  const dateSec = date.getSeconds();
+  return `${dateHour}:${dateMin}:${dateSec}`;
+}
+
+function saveToLocal() {
+  const taskContent = document.querySelector("#taskText").value;
+  return localStorage.setItem(realTime(), taskContent);
+}
