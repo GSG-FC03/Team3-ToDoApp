716ecdb41758494a026d6ff26f49f7cab8ed3fbc
adnan-afana <adnanafana0@gmail.com>
2021-07-17 03:47:11 +0300

    editting on saveToLocal function to make it get the keys and sort them Realtes #28

--- js/dom.js
+++ js/dom.js
@@ -3,6 +3,7 @@ const addTaskButton = document.getElementById("addTask");
 const writeTasks = document.getElementsByClassName("writeTasks");
 const properties = document.getElementsByClassName("properties");
 const submitTask = document.getElementById("saveTask");
+// To-Do Container elements
 writeTasks[0].style.display = "none";
 // Event Listeners
 addTaskButton.addEventListener("click", hideFab);
--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
 const search = document.getElementsByClassName("search");
 search[0].style.display = "none";
-
+// Function to disply the text area and hide the FAB button
 function hideFab() {
   if ((writeTasks[0].style.display = "none")) {
     writeTasks[0].style.display = "block";
     addTaskButton[0].style.display = "none";
   }
 }
-
-function setId(){
-    return localStorage.length + 1
+// Get te Length of local storage + 1 as a key number
+function setId() {
+  return localStorage.length + 1;
 }
-
+// store the value of text area to local storage
 function saveToLocal() {
   const taskContent = document.querySelector("#taskText").value;
-  return localStorage.setItem(setId(), taskContent);
+  localStorage.setItem(setId(), taskContent);
+  let localKeys = Object.keys(localStorage);
+  localKeys.sort(function (a, b) {
+    return a - b;
+  });
 }
