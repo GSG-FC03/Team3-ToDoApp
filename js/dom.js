f62dfee548fa8450155c31b0d16ec66f0442653e
Abdelrahman059 <Abodaljazzar2000@gmail.com>
2021-07-19 18:28:57 +0300

    editing and deleting butons done . relates #38

--- js/dom.js
+++ js/dom.js
@@ -3,7 +3,8 @@ const addTaskButton = document.getElementById("addTask");
 const writeTasks = document.getElementsByClassName("writeTasks");
 const properties = document.getElementsByClassName("properties");
 const submitTask = document.getElementById("saveTask");
-
+const removeButton = document.getElementsByClassName("delete")[0];
+const Edit = document.getElementsByClassName("edit")[0];
 // To-Do Container elements
 writeTasks[0].style.display = "none";
 // Event Listeners
